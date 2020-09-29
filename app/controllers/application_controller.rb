82a70a1cbd5f2bbbd1a395538f5a209741f15c68
cottpan <lok.mem@gmail.com>
2020-09-29 10:05:47 +0900

    exe chap1.4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
 
     def hello
-        render html: "hello, world!"
+        render html: "¡hola, mundo!"
     end
-    
+
+    def goodbye
+        render html: "good bye!"
+    end
+
 end
